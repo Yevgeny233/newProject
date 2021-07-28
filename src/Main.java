e729000f5cfcfa0ff99904de371bdbff40212795
Zhenya <mikhnevsky95@gmail.com>
2021-07-28 21:08:24 +0300

    testcommit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Zhenya and I am ready to learn Java");
 
-        
-        
+        ///// testchange
+
     }
 }
